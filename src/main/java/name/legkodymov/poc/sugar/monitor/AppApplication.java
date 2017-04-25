e5f44ca40ae87f290f25d5857a975dca6129f495
Sergei Legkodymov <legkodymov.sergey@otr.ru>
2017-04-25 22:29:09 +0300

    JPA sample class

--- src/main/java/name/legkodymov/poc/sugar/monitor/AppApplication.java
+++ src/main/java/name/legkodymov/poc/sugar/monitor/AppApplication.java
@@ -1,12 +1,55 @@
 package name.legkodymov.poc.sugar.monitor;
 
+import name.legkodymov.poc.sugar.monitor.entity.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(AppApplication.class);
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            //save some customers
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "Smith"));
+            repository.save(new Customer("Jennifer", "Palmer"));
+            repository.save(new Customer("David", "Smith"));
+
+            //fetch all
+            log.info("Customers found by findAll");
+            log.info("------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            //fetch customer by Id
+            log.info("Customers found by findOne");
+            log.info("------------------------------");
+            Customer customer1 = repository.findOne(1L);
+            log.info(customer1.toString());
+            log.info("");
+
+            //fetch by lastName
+            log.info("Customers found by findByLastName");
+            log.info("------------------------------");
+            for (Customer customer : repository.findByLastName("Smith")) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+        };
+    }
 }
---
+++ src/main/java/name/legkodymov/poc/sugar/monitor/entity/Customer.java
@@ -0,0 +1,53 @@
+package name.legkodymov.poc.sugar.monitor.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by sergei on 25/04/2017.
+ */
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    protected Customer() {
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+}
