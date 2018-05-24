27dbb350dede16a76ea57b6914fdad97c7fb42e1
Sergey Legkodymov <rutven@gmail.com>
2018-05-24 16:24:02 +0300

    Spring boot version update + lombok

--- src/main/java/name/legkodymov/poc/sugar/monitor/entity/Customer.java
+++ src/main/java/name/legkodymov/poc/sugar/monitor/entity/Customer.java
@@ -1,5 +1,8 @@
 package name.legkodymov.poc.sugar.monitor.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,45 +12,20 @@ import javax.persistence.Id;
  * Created by sergei on 25/04/2017.
  */
 @Entity
+@Data
+@NoArgsConstructor
 public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String firstName;
-    private String lastName;
 
-    protected Customer() {
-    }
+    private String firstName;
+
+    private String lastName;
 
     public Customer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
-
 }
